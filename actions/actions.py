019e44e93c8163e413b2baca8c16fe8ec9f25aa5
rogerioflh <rogeriofilho63@gmail.com>
2025-05-19 21:07:56 -0300

    update actions

--- actions/actions.py
+++ actions/actions.py
@@ -1,11 +1,23 @@
+from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-class ActionRegistrarCompra(Action):
-    def name(self):
-        return "action_registrar_compra"
+class ActionResumoConta(Action):
+    def name(self) -> Text:
+        return "utter_resumo_conta"
 
-    def run(self, dispatcher, tracker, domain):
-        produto = tracker.get_slot("produto")
-        dispatcher.utter_message(text=f"Compra do {produto} registrada!")
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        nome = tracker.get_slot("nome")
+        emprestimo = tracker.get_slot("emprestimo")
+        portabilidade = tracker.get_slot("portabilidade")
+        deposito = tracker.get_slot("deposito")
+
+        dispatcher.utter_message(text=f"Conta criada com sucesso para {nome}.")
+        dispatcher.utter_message(text=f"Portabilidade: {'sim' if portabilidade else 'não'}")
+        dispatcher.utter_message(text=f"Empréstimo: {'sim' if emprestimo else 'não'}")
+        dispatcher.utter_message(text=f"Valor depositado: R$ {deposito}")
+        
         return []
